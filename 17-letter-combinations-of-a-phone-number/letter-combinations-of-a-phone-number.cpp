594ac95236a586a09c4315ce9fe76d5421574bad
Amishi Kaushal <amishiii005@gmail.com>
2026-01-24 17:17:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (8.65%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    vector<string> result;
+    vector<string> res;
     unordered_map<char, string> phoneMap = {
         {'2', "abc"}, {'3', "def"},
         {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"},
         {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
     };
-    void solve(int ind , string& path , string& digits ){
+
+    void solve(int ind , string &path , string digits){
         if(ind == digits.size()){
-            result.push_back(path);
+            res.push_back(path);
             return;
         }
 
-        string letters = phoneMap[digits[ind]];
-        for (char ch : letters) {
+        string letter = phoneMap[digits[ind]];
+
+        for(auto ch : letter){
             path.push_back(ch);
-            solve(ind + 1, path, digits);
-            path.pop_back(); 
+            solve(ind + 1 , path , digits);
+            path.pop_back();
         }
+
     }
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
+         if (digits.empty()) return {};
 
         string path = "";
         solve(0, path, digits);
-        return result;
+        return res;
     }
 };
