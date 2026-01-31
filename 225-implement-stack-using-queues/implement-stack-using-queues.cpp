5a864bed4be320d040c04417ff6798d3a855ea99
Amishi Kaushal <amishiii005@gmail.com>
2026-01-31 16:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (58.24%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -29,7 +29,7 @@ public:
         return topi;
     }
     
-    int top() {
+    int top(){
         if(q1.empty()){
             return -1;
         }
@@ -43,11 +43,3 @@ public:
     }
 };
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
