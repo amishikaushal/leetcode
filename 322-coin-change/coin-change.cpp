8a60fda904b18820b77280060ffe7294eaa4e07e
Amishi Kaushal <amishiii005@gmail.com>
2026-02-11 12:40:20 +0530

    Time: 47 ms (34.14%) | Memory: 48.8 MB (39.70%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+
+        vector<vector<int>> dp(n , vector<int>(amount + 1 , 0));
+
+        for(int i = 0 ; i <= amount ; i++){
+            if(i % coins[0] == 0){
+                dp[0][i] = i / coins[0];
+            }
+            else{
+                dp[0][i] = 1e9;
+            }
+        }
+
+        for(int i = 1 ; i < n; i++){
+            for(int tar = 0 ; tar <= amount ; tar++){
+                int nottake = 0 + dp[i - 1][tar];
+
+
+                int taken = 1e9;
+                if(coins[i] <= tar){
+                    taken = 1 + dp[i][tar - coins[i]];
+                }
+
+
+                dp[i][tar] = min(nottake , taken);
+            }
+        }
+        int ans =  dp[n -1 ][amount];
+
+        if(ans >= 1e9){
+            return -1;
+        }
+        return ans;
+
+
+   } 
+};
