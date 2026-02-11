ee173fa5cdcfc9b6cd1b41da6ad86c083a8323e5
Amishi Kaushal <amishiii005@gmail.com>
2026-02-11 12:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (75.24%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0;
+        int ten = 0;
+
+        for(auto &bill : bills){
+            if(bill == 5){
+                five++;
+            }
+            else if(bill == 10){
+                if(five == 0){
+                    return false;
+                }
+                else{
+                    five--;
+                    ten++;
+                }
+            }
+            else{
+                if(ten >= 1 && five >= 1){
+                   five--;
+                   ten--;
+                }
+                else if(five >= 3){
+                    five -= 3;
+
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
