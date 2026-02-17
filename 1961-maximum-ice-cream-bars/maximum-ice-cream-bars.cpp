f0b6df2118be73fc51d850ef82aec1c075a07123
Amishi Kaushal <amishiii005@gmail.com>
2026-02-17 11:47:05 +0530

    Time: 34 ms (60.20%) | Memory: 80.2 MB (86.35%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int n = costs.size();
+
+        sort(costs.begin() , costs.end());
+        int cnt = 0;
+
+        for(int i = 0; i < n ; i++){
+            if(coins >= costs[i]){
+                cnt++;
+                coins -= costs[i];
+            }
+            else{
+                break;
+            }
+        }
+
+        return cnt;
+    }
+};
