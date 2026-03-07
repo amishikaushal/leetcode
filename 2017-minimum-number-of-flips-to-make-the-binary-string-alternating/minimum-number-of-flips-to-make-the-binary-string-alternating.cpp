a5d9964e21d517e2e8b9a6b988b6cb7fe6fa3cd6
Amishi Kaushal <amishiii005@gmail.com>
2026-03-07 11:13:15 +0530

    Time: 54 ms (7.60%) | Memory: 28.3 MB (42.80%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,37 +1,49 @@
 class Solution {
 public:
     int minFlips(string s) {
-        int n = s.size();
+        int n = s.length();
+
         string ss = s + s;
+        string alt1 = "";
+        string alt2 = "";
 
-        int diff1 = 0, diff2 = 0;
-        int l = 0;
-        int ans = INT_MAX;
-
-        for(int r = 0; r < 2*n; r++) {
-
-            char expected1 = (r % 2) ? '1' : '0';
-            char expected2 = (r % 2) ? '0' : '1';
-
-            if(ss[r] != expected1) diff1++;
-            if(ss[r] != expected2) diff2++;
-
-            if(r - l + 1 > n) {
-
-                char leftExpected1 = (l % 2) ? '1' : '0';
-                char leftExpected2 = (l % 2) ? '0' : '1';
-
-                if(ss[l] != leftExpected1) diff1--;
-                if(ss[l] != leftExpected2) diff2--;
-
-                l++;
+        for(int i = 0 ; i < 2 * n ;i++){
+            if(i % 2 == 0){
+                alt1 += '0';
+                alt2 += '1';
             }
-
-            if(r - l + 1 == n) {
-                ans = min(ans, min(diff1, diff2));
+            else{
+                alt1 += '1';
+                alt2 += '0';
             }
         }
 
+
+        int diff1 = 0; 
+        int diff2 = 0;
+        int ans = INT_MAX;
+        int l = 0;
+
+
+        for(int r = 0; r < 2 * n ; r++){
+            if(ss[r] != alt1[r]) diff1++;
+             if(ss[r] != alt2[r]) diff2++;
+
+
+             if(r - l + 1 > n){
+                if(ss[l] != alt1[l]) diff1--;
+                if(ss[l] != alt2[l]) diff2--;
+
+                l++;
+             }
+
+             if(r - l + 1 == n){
+                ans = min(ans , min(diff1 , diff2));
+             }
+
+        }
+
         return ans;
+
     }
 };
