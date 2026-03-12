57760ec2bb0fb32b40cf4f5ee8a42e007aa8243c
Amishi Kaushal <amishiii005@gmail.com>
2026-03-12 10:38:14 +0530

    Time: 54 ms (6.70%) | Memory: 57.7 MB (7.24%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void dfs(int  node , vector<vector<int>> &adj , vector<int> &vis ){
+        vis[node] = 1;
+
+        for(auto &it : adj[node]){
+            if(!vis[it]){
+                dfs(it , adj , vis);
+            }
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size() < n -1){
+            return -1;
+        }
+        vector<vector<int>> adj(n);
+
+
+        for(auto it : connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+
+        int cnt = 0;
+        vector<int> vis(n , 0);
+
+        for(int i = 0 ; i < n ; i++){
+            if(!vis[i]){
+                dfs(i , adj , vis);
+                cnt++;
+            }
+        }
+
+
+        return cnt - 1;
+    }
+};
