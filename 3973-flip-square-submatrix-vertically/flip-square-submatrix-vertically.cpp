49ef1ddced355b901c1ce10c48b1e704af4f4923
Amishi Kaushal <amishiii005@gmail.com>
2026-03-21 15:32:16 +0530

    Time: 7 ms (6.02%) | Memory: 33.9 MB (31.02%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
-        int sc = y;
-        int ec = y + k - 1;
-        int sr = x;
-        int er = x + k - 1;
-        for (int j = sc; j <= ec; j++) {
-            for (int i = 0; i < k / 2; i++) {
-                swap(grid[sr+i][j], grid[sr+k-i-1][j]);
+        for(int i = 0 ; i < k / 2 ; i++){
+            for(int j = 0 ; j < k ; j++){
+                swap(grid[x+i][y+j] , grid[x + k - 1 - i ][y + j]);
             }
         }
+
         return grid;
     }
 };
