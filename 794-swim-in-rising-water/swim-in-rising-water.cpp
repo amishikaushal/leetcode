d80a67c00847e707fb5927627863fb94200e3654
Amishi Kaushal <amishiii005@gmail.com>
2026-03-23 11:30:38 +0530

    Time: 19 ms (23.17%) | Memory: 14.1 MB (48.43%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -3,40 +3,40 @@ public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
 
-        vector<vector<bool>> vis(n , vector<bool>(n , false));
+        vector<vector<int>> vis(n , vector<int> (n , 0));
 
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        priority_queue<tuple<int , int , int> , vector<tuple<int , int , int>> , greater<>> pq;
 
         pq.emplace(grid[0][0] , 0 , 0);
-
-        vector<int> dr = {-1 , 0 , 1, 0};
+        vector<int> dr = {-1 , 0 , 1 , 0};
         vector<int> dc = {0 , -1 , 0 , 1};
 
+
         while(!pq.empty()){
-            auto [time , row , col] = pq.top();
+            auto [time , r , c] = pq.top();
             pq.pop();
 
-            if(row == n -1 && col == n -1){ 
+            if (vis[r][c]) continue;  
+            vis[r][c] = 1;
+
+            if(r == n -1 && c == n -1){
                 return time;
             }
 
-            if(vis[row][col] == true) continue;
-
-            vis[row][col] = true;
-
-
 
             for(int i = 0 ; i < 4 ; i++){
-                int newr = row + dr[i];
-                int newc = col + dc[i];
+                int newr = r + dr[i];
+                int newc = c + dc[i];
 
-                if(newr >= 0 && newc < n && newr < n && newc >= 0){
+
+                if(newr >= 0 && newr < n && newc >= 0 && newc < n){
                     pq.emplace(max(time , grid[newr][newc]) , newr , newc);
                 }
             }
 
-            
         }
+
         return -1;
+
     }
 };
