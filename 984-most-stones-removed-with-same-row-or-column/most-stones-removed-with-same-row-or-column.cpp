8ccc6fb584329b6315e458d35e8cc5e34aed9c59
Amishi Kaushal <amishiii005@gmail.com>
2026-03-30 16:57:53 +0530

    Time: 88 ms (9.46%) | Memory: 18.2 MB (92.29%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -3,7 +3,7 @@ public:
     void dfs(int ind , vector<vector<int>>& stones , vector<int> &vis){
         vis[ind] = 1;
 
-        for(int i = 0 ; i < stones.size() ; i++){
+        for(int i = 0; i < stones.size() ; i++){
             if(!vis[i]){
                 if(stones[i][0] == stones[ind][0] || stones[i][1] == stones[ind][1]){
                     dfs(i , stones , vis);
@@ -15,15 +15,15 @@ public:
         int n = stones.size();
 
         vector<int> vis(n , 0);
-        int components = 0;
+        int cnt = 0;
 
         for(int i = 0; i < n ; i++){
             if(!vis[i]){
-                dfs(i ,  stones , vis);
-                components++;
+                dfs(i , stones , vis);
+                cnt++;
             }
         }
 
-        return n - components;
+        return n - cnt;
     }
 };
