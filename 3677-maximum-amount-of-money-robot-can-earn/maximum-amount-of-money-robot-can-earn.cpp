97040715b5746cb33aa3b410cc003205da88da70
Amishi Kaushal <amishiii005@gmail.com>
2026-04-02 12:00:45 +0530

    Time: 358 ms (73.19%) | Memory: 108.3 MB (83.83%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -1,69 +1,55 @@
 class Solution {
 public:
+    int dp[505][505][3];
+
+    int solve(int i, int j, int k, vector<vector<int>>& coins, int m, int n) {
+        
+        if(i >= m || j >= n) return INT_MIN;
+
+        if(i == m-1 && j == n-1){
+            if(coins[i][j] < 0 && k > 0) return 0;
+            return coins[i][j];
+        }
+
+        if(dp[i][j][k] != INT_MIN) return dp[i][j][k];
+
+        int val = coins[i][j];
+
+        int down = solve(i+1, j, k, coins, m, n);
+        int right = solve(i, j+1, k, coins, m, n);
+
+        int bestNext = max(down, right);
+
+        int ans = INT_MIN;
+
+        if(bestNext != INT_MIN){
+            ans = val + bestNext;
+        }
+
+        // skip
+        if(val < 0 && k > 0){
+            int downSkip = solve(i+1, j, k-1, coins, m, n);
+            int rightSkip = solve(i, j+1, k-1, coins, m, n);
+
+            int bestSkip = max(downSkip, rightSkip);
+
+            if(bestSkip != INT_MIN){
+                ans = max(ans, bestSkip);
+            }
+        }
+
+        return dp[i][j][k] = ans;
+    }
+
     int maximumAmount(vector<vector<int>>& coins) {
-        int m = coins.size();
-        int n = coins[0].size();
+        int m = coins.size(), n = coins[0].size();
 
-        vector<vector<vector<int>>> dp(m , vector<vector<int>>(n , vector<int>(3 , INT_MIN)));
+        // initialize with INT_MIN
+        for(int i=0;i<505;i++)
+            for(int j=0;j<505;j++)
+                for(int k=0;k<3;k++)
+                    dp[i][j][k] = INT_MIN;
 
-        for(int k = 0; k <= 2 ; k++){
-            if(coins[0][0] >= 0){
-                dp[0][0][k] = coins[0][0];
-            }
-            else{
-                dp[0][0][k] = coins[0][0];
-                if(k > 0){
-                    dp[0][0][k] = max(dp[0][0][k] , 0);
-                }
-            }
-        }
-
-
-
-        for(int i = 0; i < m ; i++){
-            for(int j = 0; j < n ; j++){
-                if(i == 0 && j == 0 ) continue;
-
-
-                for(int k = 0; k <= 2 ; k++){
-                    int best = INT_MIN;
-
-                    if(i > 0){
-                        best = max(best , dp[i-1][j][k]);
-                    }
-
-                    if(j > 0){
-                        best = max(best , dp[i][j-1][k]);
-                    }
-
-
-                    if(best == INT_MIN) continue;
-
-
-                    dp[i][j][k] = best + coins[i][j];
-
-
-                    if(coins[i][j] < 0 && k > 0){
-                        int bestPrev = INT_MIN;
-
-                        if(i > 0){
-                            bestPrev = max(bestPrev , dp[i-1][j][k-1]);
-
-                        }
-
-                        if(j > 0){
-                            bestPrev = max(bestPrev , dp[i][j-1][k-1]);
-                        }
-
-
-                        if(bestPrev != INT_MIN){
-                            dp[i][j][k] = max(dp[i][j][k] , bestPrev);
-                        }
-                    }
-                }
-            }
-        }
-
-        return max({dp[m-1][n-1][0] , dp[m-1][n-1][1] , dp[m-1][n-1][2]});
+        return solve(0, 0, 2, coins, m, n);
     }
 };
