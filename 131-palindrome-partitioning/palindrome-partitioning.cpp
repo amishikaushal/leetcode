b30576d8375cda307ae91137c6ca90f60f762cc1
Amishi Kaushal <amishiii005@gmail.com>
2026-04-16 15:37:38 +0530

    Time: 183 ms (5.00%) | Memory: 175.5 MB (5.01%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    vector<vector<string>> ans;
     bool isPalindrome(string s , int st , int e){
         while(st <= e){
             if(s[st++] != s[e--]){
@@ -8,7 +9,7 @@ public:
         }
         return true;
     }
-    void solve(int ind , string s, vector<string> res,  vector<vector<string>> &ans){
+    void solve(int ind , string s, vector<string> res){
         if(ind == s.length()){
             ans.push_back(res);
             return;
@@ -17,16 +18,16 @@ public:
         for(int i = ind ; i < s.length() ; i++){
             if(isPalindrome(s , ind , i)){
                 res.push_back(s.substr(ind , i - ind + 1));
-                solve(i + 1 , s , res , ans);
+                solve(i + 1 , s , res );
                 res.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
+      
         vector<string> res;
 
-        solve(0 , s , res , ans);
+        solve(0 , s , res);
         return ans;
     }
 };
