1b79d5cd16de3ed1332748d93a0c5515b09480b4
Amishi Kaushal <amishiii005@gmail.com>
2026-06-03 11:54:19 +0530

    Time: 4 ms (14.52%) | Memory: 22 MB (58.22%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -4,25 +4,26 @@ public:
         stack<int> st;
         vector<int> ans;
 
-        for(auto &it : asteroids){
+        for(auto it : asteroids){
             if(it > 0){
                 st.push(it);
             }
             else{
+                
+
                 while(!st.empty() && st.top() > 0 && st.top() < abs(it)){
                     st.pop();
                 }
-
 
                 if(!st.empty() && st.top() == abs(it)){
                     st.pop();
                 }
 
-
                 else if(st.empty() || st.top() < 0){
                     st.push(it);
-                } 
+                }
             }
+
         }
 
         while(!st.empty()){
@@ -31,7 +32,6 @@ public:
         }
 
         reverse(ans.begin() , ans.end());
-
         return ans;
     }
 };
