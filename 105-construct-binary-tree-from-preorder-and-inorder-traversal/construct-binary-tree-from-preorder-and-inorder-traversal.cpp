dc4916d31c6b9efe492d7e141b598af3ed7243b1
Amishi Kaushal <amishiii005@gmail.com>
2026-06-17 12:07:10 +0530

    Time: 2 ms (78.61%) | Memory: 27.5 MB (53.83%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,37 +11,34 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int , int> mpp;
-
-        int n = inorder.size();
-
-        for(int i = 0; i < n ; i++){
-            mpp[inorder[i]] = i;
-        }
-
-        TreeNode* root = solve(preorder , 0 , n -1 , inorder , 0 , n -1 , mpp);
-
-        return root;
-    }
-
-    TreeNode* solve(vector<int>& preorder , int pres , int preend , vector<int>& inorder , int ins , int inend , map<int , int> &mpp){
-        if(pres > preend || ins > inend){
+    TreeNode* solve(vector<int>& preorder, int pres , int prend, vector<int>& inorder, int ins , int inend , map<int , int> &mpp){
+        if(pres > prend || ins > inend){
             return nullptr;
         }
-
 
         TreeNode* root = new TreeNode(preorder[pres]);
 
         int inRoot = mpp[root -> val];
+
         int lefti = inRoot - ins;
 
-        root -> left = solve(preorder , pres + 1, pres + lefti , inorder , ins , inRoot - 1 , mpp);
+        root -> left = solve(preorder , pres + 1 , pres + lefti , inorder , ins , inRoot - 1 , mpp);
 
 
-        root -> right = solve(preorder , pres + lefti + 1 , preend , inorder , inRoot + 1 , inend , mpp);
-
+        root -> right = solve(preorder , pres + lefti + 1  , prend , inorder ,  inRoot + 1 , inend, mpp);
 
         return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int , int> mpp;
+        int n = inorder.size();
+
+        for(int i = 0; i < n; i++){
+            mpp[inorder[i]] = i;
+        }
+
+
+        return solve(preorder , 0 , n -1 , inorder , 0 , n -1 , mpp);
+
     }
 };
