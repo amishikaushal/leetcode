8e0cd91b1c1b56dee8650553d8580d86745257be
Amishi Kaushal <amishiii005@gmail.com>
2026-06-22 17:32:48 +0530

    Time: 811 ms (25.11%) | Memory: 11.6 MB (38.81%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    bool search(int i , int j , vector<vector<char>>& board, string word , int ind , int n , int m){
-        if(ind == word.length()){
+    bool solve(int i , int j , vector<vector<char>>& board, string word , int ind , int n , int m ){
+        if(ind == word.size()){
             return true;
         }
 
-        if(i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[ind]) return false;
+        if(i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[ind]){
+            return false;
+        }
 
+        char c = word[ind];
 
-        char c = board[i][j];
         board[i][j] = '.';
 
-        int l =search(i , j + 1 , board , word , ind + 1 , n , m);
-        int r = search(i  , j - 1 , board , word , ind + 1 , n , m);
-        int t = search(i -1 , j , board , word , ind + 1 , n , m);
-        int b = search(i + 1 , j , board , word , ind + 1 , n , m);
+        bool l = solve(i , j + 1 , board , word , ind + 1 , n , m);
+        bool r = solve(i , j - 1 , board , word , ind + 1 , n , m);
+        bool u = solve(i - 1 , j , board , word , ind + 1 , n , m);
+        bool d = solve(i + 1, j  , board , word , ind + 1 , n , m);
+
 
         board[i][j] = c;
 
-        return l || r || t || b;
-
+        return l || r || u || d;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
         int m = board[0].size();
+        int n = board.size();
 
 
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 0; i < n ; i++){
             for(int j = 0; j < m ; j++){
                 if(board[i][j] == word[0]){
-                    if(search(i , j , board , word , 0 , n , m)){
+                    if(solve(i , j , board , word , 0 , n , m)){
                         return true;
                     }
                 }
