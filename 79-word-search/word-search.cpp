fa6f108fbe34f3e4a0ab2fd974877e712f7a3f49
Amishi Kaushal <amishiii005@gmail.com>
2026-07-21 13:03:00 +0530

    Time: 827 ms (21.32%) | Memory: 11.6 MB (38.08%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool solve(int i , int j , vector<vector<char>>& board, string word , int ind , int n , int m ){
+    bool solve(int i , int j , vector<vector<char>>& board, string word, int ind , int n , int m){
         if(ind == word.size()){
             return true;
         }
@@ -9,29 +9,29 @@ public:
             return false;
         }
 
-        char c = word[ind];
+        char c = board[i][j];
 
         board[i][j] = '.';
 
-        bool l = solve(i , j + 1 , board , word , ind + 1 , n , m);
-        bool r = solve(i , j - 1 , board , word , ind + 1 , n , m);
-        bool u = solve(i - 1 , j , board , word , ind + 1 , n , m);
-        bool d = solve(i + 1, j  , board , word , ind + 1 , n , m);
+        bool l = solve(i + 1 , j , board , word , ind + 1 , n , m);
+        bool r = solve(i - 1 , j , board , word , ind + 1 , n , m);
+        bool u = solve(i  , j + 1 , board , word , ind + 1 , n , m);
+        bool d = solve(i , j - 1, board , word , ind + 1 , n , m);
 
 
         board[i][j] = c;
 
         return l || r || u || d;
+
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board[0].size();
         int n = board.size();
-
+        int m = board[0].size();
 
         for(int i = 0; i < n ; i++){
             for(int j = 0; j < m ; j++){
                 if(board[i][j] == word[0]){
-                    if(solve(i , j , board , word , 0 , n , m)){
+                    if(solve(i , j , board , word , 0 , n , m )){
                         return true;
                     }
                 }
