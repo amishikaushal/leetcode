8b556071185d1a9baabca598d5392574c4dc328f
Amishi Kaushal <amishiii005@gmail.com>
2026-07-21 12:32:38 +0530

    Time: 25 ms (70.50%) | Memory: 36.7 MB (26.49%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -9,68 +9,70 @@
  */
 class Codec {
 public:
-    
-    
+
+    // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(!root) return "";
-
-    string ans = "";
-    queue<TreeNode*> q;
-    q.push(root);
-
-    while(!q.empty()) {
-        TreeNode* curr = q.front();
-        q.pop();
-
-        if(curr == nullptr) {
-            ans += "null ";
-            continue;
+        if(!root){
+            return "";
         }
 
-        ans += to_string(curr->val) + " ";
-        q.push(curr->left);
-        q.push(curr->right);
+        string ans = "";
+        queue<TreeNode*> q;
+        q.push(root);
+
+
+        while(!q.empty()){
+            TreeNode* curr= q.front();
+            q.pop();
+
+            if(curr == nullptr){
+                ans += "null ";
+                continue;
+            }
+
+            ans += to_string(curr -> val) + " ";
+            q.push(curr -> left);
+            q.push(curr -> right);
+        }
+        return ans;
     }
 
-    return ans;
-    }
-
-    // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-         if(data.size() == 0) return nullptr;
+        if(data.size() == 0) return nullptr;
 
         stringstream ss(data);
         vector<string> val;
         string temp;
 
-        while(ss >> temp)
+
+        while(ss >> temp){
             val.push_back(temp);
+        }
 
         TreeNode* root = new TreeNode(stoi(val[0]));
         queue<TreeNode*> q;
         q.push(root);
 
+
         int i = 1;
 
-        while(!q.empty() && i < val.size()) {
+        while(!q.empty() && i < val.size()){
             TreeNode* curr = q.front();
             q.pop();
 
-            if(val[i] != "null") {
-                curr->left = new TreeNode(stoi(val[i]));
-                q.push(curr->left);
+            if(val[i] != "null"){
+                curr -> left = new TreeNode(stoi(val[i]));
+                q.push(curr -> left);
             }
             i++;
 
-            if(i < val.size() && val[i] != "null") {
-                curr->right = new TreeNode(stoi(val[i]));
-                q.push(curr->right);
+            if(i < val.size() && val[i] != "null"){
+                curr -> right = new TreeNode(stoi(val[i]));
+                q.push(curr -> right);
             }
             i++;
         }
-
         return root;
-
     }
 };
 
