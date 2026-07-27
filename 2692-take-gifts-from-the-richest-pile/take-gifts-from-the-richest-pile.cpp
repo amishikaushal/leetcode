52b82a8eb94ed92e1b5792217588fc768648572e
Amishi Kaushal <amishiii005@gmail.com>
2026-07-27 14:21:01 +0530

    Time: 3 ms (43.37%) | Memory: 13 MB (61.72%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq;
+
+        for(auto it : gifts){
+            pq.push(it);
+        }
+
+        while(k){
+            int topi = pq.top();
+            pq.pop();
+
+            int ele = floor(sqrt(topi));
+            pq.push(ele);
+            k--;
+            
+        }
+
+        long long sum = 0;
+
+        while(!pq.empty()){
+            sum += pq.top();
+            pq.pop();
+        }
+
+        return sum;
+    }
+};
