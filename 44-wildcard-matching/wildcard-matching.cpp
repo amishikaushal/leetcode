04a3ce9593af421858de68493993dd94e9dbc4ce
Amishi Kaushal <amishiii005@gmail.com>
2026-07-28 15:05:13 +0530

    Time: 56 ms (43.10%) | Memory: 54.7 MB (18.00%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -4,30 +4,34 @@ public:
         int n = s.length();
         int m = p.length();
 
-        vector<vector<bool>> dp(n + 1 , vector<bool>(m + 1 , false));
 
+        vector<vector<int>> dp(n+ 1 , vector<int>(m+1 , false));
 
         dp[0][0] = true;
 
-        for(int j = 1 ; j <= m ; j++){
-            if(p[j-1] == '*'){
-                dp[0][j] = dp[0][j-1];
+
+        for(int i = 1 ; i <= m ; i++){
+            if(p[i-1] == '*'){
+                dp[0][i] = dp[0][i-1];
             }
         }
 
 
-        for(int i = 1 ; i <= n ; i++){
+        for(int i = 1; i <= n ; i++){
             for(int j = 1 ; j <= m ; j++){
-                if((s[i-1] == p[j-1]) || p[j-1] == '?'){
+                if(s[i-1] == p[j-1] || p[j-1] == '?'){
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else if(p[j-1] == '*'){
-                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
-                }else{
-                    dp[i][j] = false;
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
                 }
-            } 
+                else{
+                    dp[i][j] = false;
+
+                }
+            }
         }
+
         return dp[n][m];
     }
 };
