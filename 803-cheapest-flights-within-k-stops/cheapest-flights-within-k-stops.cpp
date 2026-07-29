886b60f0a706c9a09571be5d68cb5519be5994e7
Amishi Kaushal <amishiii005@gmail.com>
2026-07-29 12:39:00 +0530

    Time: 4 ms (44.88%) | Memory: 18.4 MB (31.42%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,45 +1,52 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+
         vector<vector<pair<int , int>>> adj(n);
 
-        for (auto it : flights) {
+        for(auto it : flights){
             int from = it[0];
             int to = it[1];
             int price = it[2];
+
             adj[from].push_back({to , price});
         }
 
-     
-        priority_queue<pair<int , pair<int , int>>,
-                       vector<pair<int , pair<int , int>>>,
-                       greater<pair<int , pair<int , int>>>> pq;
 
-        pq.push({0, {src, 0}});
+        priority_queue<pair<int, pair<int, int>>, 
+                       vector<pair<int, pair<int, int>>>, 
+                       greater<pair<int, pair<int, int>>>> pq;
 
-        vector<int> dist(n, 1e9);
+        vector<int> dist(n , 1e9);
+
         dist[src] = 0;
+        pq.push({0 , {src , 0}});
 
-        while (!pq.empty()) {
-            auto itt = pq.top();
+        while(!pq.empty()){
+            int steps = pq.top().first;
+            int node = pq.top().second.first;
+            int dis = pq.top().second.second;
+
             pq.pop();
 
-            int steps = itt.first;
-            int node  = itt.second.first;
-            int cost  = itt.second.second;
+            if(steps > k){
+                continue;
+            }
 
-            if (steps > k) continue;
+            for(auto it : adj[node]){
+                int nei = it.first;
+                int cost = it.second;
 
-            for (auto neigh : adj[node]) {
-                int nextNode = neigh.first;
-                int wt = neigh.second;
+                if(dist[nei] > cost + dis){
+                    dist[nei] = cost + dis;
 
-                if (cost + wt < dist[nextNode]) {
-                    dist[nextNode] = cost + wt;
-                    pq.push({steps + 1, {nextNode, dist[nextNode]}});
+                    pq.push({steps +1 , {nei , dist[nei]}});
                 }
             }
         }
         return dist[dst] == 1e9 ? -1 : dist[dst];
+
+
     }
 };
