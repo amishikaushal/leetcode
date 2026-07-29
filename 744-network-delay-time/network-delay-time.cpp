f3cda2d7c62f7344b6d6ed5c8f0dee6d042fdbb2
Amishi Kaushal <amishiii005@gmail.com>
2026-07-29 13:39:06 +0530

    Time: 87 ms (43.00%) | Memory: 48.1 MB (9.40%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-         vector<vector<pair<int , int>>> adj(n+ 1);
+        vector<vector<pair<int , int>>> adj(n + 1);
 
-        for(auto &it : times){
-            int u = it[0];
-            int v = it[1];
-            int wt = it[2];
 
-            adj[u].push_back({v , wt});
+        for(auto it : times){
+            int to = it[0];
+            int from = it[1];
+            int cost = it[2];
+
+            adj[to].push_back({from , cost});
+
         }
 
-        vector<int> dist(n + 1, INT_MAX);
+        priority_queue<pair<int , int> , vector<pair<int , int>> , greater<pair<int , int>>> pq;
 
-        priority_queue<pair<int , int> , vector<pair<int, int>> , greater<pair<int , int>>> pq;
+        vector<int> dist(n + 1, 1e9);
+
+        dist[k] = 0;
 
         pq.push({0 , k});
-        dist[k] = 0;
 
         while(!pq.empty()){
             int dis = pq.top().first;
@@ -24,27 +27,24 @@ public:
             pq.pop();
 
 
+            for(auto it : adj[node]){
+                int cost = it.second;
+                int nei = it.first;
 
-            for(auto &it : adj[node]){
-                int neigh = it.first;
-                int wt = it.second;
-
-                if(dis + wt < dist[neigh]){
-                    dist[neigh] = dis + wt;
-                    pq.push({dist[neigh] , neigh});
+                if(cost +  dis < dist[nei]){
+                    dist[nei] = cost +  dis;
+                    pq.push({dist[nei] , nei});
                 }
             }
         }
 
-        int maxi = 0;
+        int maxi = INT_MIN;
         for(int i = 1 ; i <= n ; i++){
-            if(dist[i] == INT_MAX) return -1;
+            if(dist[i] == 1e9){
+                return -1;
+            }
             maxi = max(maxi , dist[i]);
         }
-
         return maxi;
-
     }
-
-
 };
