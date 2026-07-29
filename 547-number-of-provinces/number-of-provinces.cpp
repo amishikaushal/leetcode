c47697ee109adfdf595c95cca173e76eaa13df4b
Amishi Kaushal <amishiii005@gmail.com>
2026-07-29 15:54:30 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (9.28%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,58 +1,41 @@
-class DSU{
-public:
-    vector<int> size , parent;
-
-    DSU(int n){
-        parent.resize(n);
-        size.resize(n , 1);
-        iota(parent.begin() , parent.end() , 0);
-    }
-
-
-    int find(int node){
-        if(parent[node] == node) return node;
-        return parent[node] = find(parent[node]);
-    }
-
-    void unite(int u , int v){
-        int pu = find(u);
-        int pv = find(v);
-
-        if(pu == pv) return;
-
-        if(size[pu] < size[pv]){
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        }
-        else{
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        }
-    }
-};
-
-
-
 class Solution {
 public:
+    void dfs(int i , vector<vector<int>> &adj , vector<int> &vis){
+        vis[i] = 1;
+
+        for(auto it : adj[i]){
+            if(!vis[it]){
+                dfs(it , adj , vis);
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        DSU d(n);
+
+
+        vector<vector<int>> adj(n);
+
         for(int i = 0; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
-                if(isConnected[i][j]){
-                    d.unite(i , j);
+            for(int j = 0 ; j < n ; j++){
+                if(isConnected[i][j] == 1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+
                 }
             }
         }
 
+
         int cnt = 0;
+        vector<int> vis(n , 0);
+
         for(int i = 0; i < n ; i++){
-            if(d.find(i) == i) cnt++;
+            if(!vis[i]){
+                dfs(i , adj, vis);
+                cnt++;
+            }
         }
 
         return cnt;
-
-
     }
 };
